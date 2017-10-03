797d682f8dc44c6fc1b8d8740dee71a775e56528
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-03 10:54:05 +0800

    conflict fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-<<<<<<< HEAD
   validates :password, presence: true
-=======
   validates :email, presence: true,
     uniqueness: true
->>>>>>> master
 end
